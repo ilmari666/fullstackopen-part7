0beb92ee74822e2caeab380828ea88d85f8591b8
Kuula Ilmari V <ikuula@cubbli18-gold.local>
2018-10-11 15:54:10 +0300

    tweak css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,21 +9,30 @@ import UserInfo from './UserInfo';
 
 const GradientBox = styled.div`
   position: float;
-  margin-top: 0px;
-  padding: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding: 0px;
   width: 100%;
   height: 50px;
   background: linear-gradient(to bottom, #7abcff 0%, #60abf8 44%, #4096ee 100%);
 `;
+
 const Navi = styled.div`
   position: float;
-  margin-top: 0px;
-  padding: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
   width: 100%;
   height: 12px;
-  background: linear-gradient(to bottom, #7abcff 0%, #60abf8 44%, #4096ee 100%);
+  background: linear-gradient(to bottom, rgba(244,244,244,1) 17%,rgba(229,229,229,1) 81%);
 `;
-
+const Line = styled.div`
+  background: #aaaaaa;
+  height: 1px;
+  padding: 0px;
+  margin-right: 0px;
+  width: 100%;
+`;
 
 const Header = props => (
   <>
@@ -35,6 +44,7 @@ const Header = props => (
       <Link to="/blogs">Blogs</Link> &nbsp;
       <Link to="/about">About</Link> &nbsp;
     </Navi>
+    <Line />
   </>
 );
 
